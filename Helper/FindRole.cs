583c897fce1614e31210442ab8bdb4b8a40bc4a3
AirapetyanAlbert <93880096+AirapetyanAlbert@users.noreply.github.com>
2021-12-12 23:49:52 +0300

    Add files via upload

---
+++ Helper/FindRole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApplDemo2018.Model;
+
+namespace WpfApplDemo2018.Helper
+{
+    public class FindRole
+    {
+        int id;
+        public FindRole(int id)
+        {
+            this.id = id;
+        }
+        public bool RolePredicate(Role role)
+        {
+            return role.Id == id;
+        }
+    }
+
+}
